bdc5ea165389e61fc554cb83647b29d9c0708a61
Zack Eisbach <zack.eisbach@gmail.com>
2025-03-05 14:48:49 -0500

    starting to solve

--- runtime.rkt
+++ runtime.rkt
@@ -19,7 +19,8 @@
 ;; A Datum is RacketDatum
 ;; Ideally, we would allow any Racket datum here, but this may change
 ;; if implementing this is infeasible.
-;; TODO : maybe revisit this
+;; CURRENTLY, it seems as if we will need to be able to compare for equality
+;; to compare attributes and determine when conflicts arise
 
 ;; A Fact is a (fact Symbol [ListOf Datum] [Option Datum]).
 ;; It represents a known fact (either given or deduced) in the database.
@@ -34,6 +35,7 @@
   (fact name terms value))
 
 ;; A Database is a [ListOf Fact]
+;; TODO: make this something with more structure for faster lookup
 
 ;; A Variable is a (variable Symbol)
 (struct variable [name])
@@ -43,9 +45,12 @@
 ;; - Variable
 
 ;; A RuleFragment is (rule-frag Symbol [ListOf Term] [ListOf Term]).
-;; It is an internal representation of the source syntax, which in
+;; It is an _internal representation_ of the source syntax, which in
 ;; general may contain variables. These can be combined to form rules
-;; or facts (closed rules with no premises)
+;; or facts (closed rules with no premises).
+;; Notably, a Term is an immediate, and does not have nested terms:
+;; this is a difference from the surface <term> syntax; these nested
+;; terms will be desugared in an ANF-like pass.
 
 (struct rule-frag [name terms choices])
 
@@ -60,10 +65,12 @@
 ;; define-logic binds the identifier to a `Logic`
 ;; `Logic` is an opaque object that can be used to obtain solutions.
 
-;; Internally, we know that a Logic is a (logic [ListOf Rule])
+;; Internally, we know that a Logic is a (logic [ListOf Rule] [ListOf Rule])
+;; storing the rules which never require making a choice in the conclusion
+;; and the rules which require making choices (> 1 option on RHS of `is`)
 ;; TODO: add in information about imported Racket function(al relation)s
 
-(struct logic [rules])
+(struct logic [deduce-rules choose-rules])
 
 ;; A Solution is an opaque object that can be queried for propositions.
 
@@ -93,7 +100,7 @@
 ;; A SearchState is a (search-state Database RuleFragment [SetOf Nat]).
 ;; It represents information associated with a choice point: namely, the facts
 ;; known to be true at the time of choice, the options to choose from
-;; (a now-grounded conclusion), and the indices of choices which have been made.
+;; (a now-grounded conclusion), and indices of choices which have been made.
 
 (struct search-state [database conclusion tried])
 
@@ -105,6 +112,12 @@
 ;; (choose) Database Logic SearchStack -> Database SearchStack
 
 ;; A SolutionResult is [Maybe [PairOf Solution SearchStack]]
+
+;; An ConsistencyResult is one of:
+;; - 'inconsistent
+;; - #f
+;; - [PairOf Solution SearchStack]
+;; and represents a SolutionResult where an inconsistency may have been reached
 
 ;; sample: Logic -> [Maybe Solution]
 ;; Obtain one possible solution of the given program, if one exists.
@@ -122,18 +135,37 @@
     (match x
       [#f (empty-stream)]
       [(cons sol next-st) (stream-cons sol (collect-backtracked next-st))]))
-  
   ; SearchStack -> [StreamOf Solution]
   (define (collect-backtracked stack)
     (result->stream (backtrack prog stack)))
+  
   (result->stream (solve prog '() '())))
 
-;; solve: Logic SearchStack Database -> SolutionResult
+;; solve: Logic Database SearchStack -> SolutionResult
 ;; Given a search state and a database of currently known facts, obtain a
-;; single solution to the program, while tracking choices made.
+;; single solution to the program, while tracking choices made. 
 
-(define (solve prog stack db)
-  #f)
+(define (solve prog db stack)
+  ; if there is an inconsistency reached, we backtrack in an attempt
+  ; to get to a better spot in our tree. if all things are inconsistencies,
+  ; then backtrack and solve will call each other and eventually return #f
+
+  ;; TODO: clean up. this is clear, but ugly...
+  ;; also, all calls are in tail position, to prevent stack overflow
+  (match (deduce prog db stack)
+    [(cons new-db new-st) (solve (logic-deduce-rules prog) new-db new-st)]
+    ['inconsistent ; triggers a backtrack
+     (backtrack prog stack)]
+    [#f
+     ;; if we could not deduce, we instead try to choose
+     (match (choose prog db stack)
+       [(cons new-db new-st) (solve (logic-choose-rules prog) new-db new-st)]
+       ['inconsistent ; triggers a backtrack
+        (backtrack prog stack)]
+       [#f
+        ;; if no new deductions or choices are left to be made, we are
+        ;; saturated and have reached a solution!
+        (cons db stack)])]))
 
 ;; backtrack: Logic SearchStack -> SolutionResult
 ;; Backtrack from the given search state and find a new solution to the given
@@ -174,8 +206,36 @@
         (if (empty? untried) (backtrack prog (rest stack))
             (let ([choice-idx (random (length untried))])
               (solve prog
-                     (update-stack stack choice-idx)
-                     (add-to-db current-state choice-idx)))))))
+                     (add-to-db current-state choice-idx)
+                     (update-stack stack choice-idx)))))))
+
+;; deduce : [ListOf Rule] Database SearchStack -> ConsistencyResult
+;; Makes one deduction, using the first deduction rule which fires, if
+;; possible. Returns #f if no rules can fire, and 'inconsistent if an
+;; inconsistency arises (which will trigger backtracking)
+;; Since no choices are made, the SearchStack is threaded through
+
+(define (deduce rules db stack)
+  (match rules
+    ['() #f]
+    [(cons rule rules)
+     (match (deduce-with-rule rule db stack)
+       [#f (deduce rules db stack)]
+       ;; propogates inconsistency, or returns the new good db + stack
+       [res res])]))
+
+;; deduce-with-rule : Rule Database SearchStack -> ConsistencyResult
+;; Attempts to use the given rule to make a new deduction
+;; If no deductions can be made, #f; if inconsistent, 'inconsistent
+
+(define (deduce-with-rule rule db stack) #f)
+
+;; choose : [ListOf Rule] Database SearchStack -> ConsistencyResult
+;; Makes one choice if possible, updating the known facts and SearchStack
+;; if no choices are left to be made, returns #f. If inconsistency arises,
+;; then 'inconsistent
+
+(define (choose prog db stack) #f)
 
 ;; has: Solution Symbol Datum ... -> Bool
 ;; Returns `#t` if the given proposition exists in this solution.

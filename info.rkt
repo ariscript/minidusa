c5018a30a37dacb446538a6c462726fe6d564c26
Ari Prakash <ariprakash16@gmail.com>
2025-03-15 17:28:49 -0400

    make build?

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang info
 (define collection "minidusa")
-(define deps '("base"))
+(define deps '("base" "syntax-spec-v3"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/minidusa.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "Finite-choice logic DSL in Racket, inspired by Dusa")
 (define version "0.0")
 (define pkg-authors '(ari))
 (define license '(AGPL-3.0-only))

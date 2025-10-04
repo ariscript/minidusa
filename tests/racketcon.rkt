63e0301d0d96577f212bd8ed95df2fec049e929d
Zack Eisbach <zack.eisbach@gmail.com>
2025-10-04 11:08:26 -0400

    ex

--- tests/racketcon.rkt
+++ tests/racketcon.rkt
@@ -224,10 +224,10 @@
         (participant "Ryan" "06/20/2004")
         (participant "Ari" "01/01/1995")))
 
-;; participants->factset : [SetOf Participant] -> [SetOf Fact]
-(define (participants->factset ps)
+;; participants->factset : [SetOf Participant] Symbol -> [SetOf Fact]
+(define (participants->factset ps name)
   (for/set ([p ps])
-    (fact 'person (list (participant-name p)) (participant-DOB p))))
+    (fact name (list (participant-name p)) (participant-DOB p))))
 
 ;; same-year? : DateString DateString -> Bool
 (define (same-year? dob1 dob2)
@@ -235,13 +235,21 @@
     (->year (parse-date dob "M/d/yyyy")))
   (equal? (extract-year dob1) (extract-year dob2)))
 
-(define dob-ex
-  (logic #:import ([year=? same-year?]) #:extern (person)
-    ((edge X Y) :- ((person X) is DOB1)
-                   ((person Y) is DOB2)
-                   ((year=? DOB1 DOB2) is #t))))
-
-#;(soln->factset
+(soln->factset
  (stream-first
-  (solve #:facts (participants->factset (fetch-participants))
-    dob-ex)))
+  (solve #:facts (participants->factset (fetch-participants) 'person)
+    (logic #:import ([year=? same-year?]) #:extern (person)
+      ((edge X Y) :- ((person X) is DOB1)
+                     ((person Y) is DOB2)
+                     ((year=? DOB1 DOB2) is #t))
+      
+      (((room X) is {'room1 'room2 'room3}) :- (edge X _))
+      (forbid ((room X) is R)
+              ((room Y) is R)
+              (edge X Y))))))
+
+#;(logic #:import ([year=? same-year?]) #:extern (person)
+  ((edge X Y) :- ((person X) is DOB1)
+                 ((person Y) is DOB2)
+                 ((year=? DOB1 DOB2) is #t))
+  #| more computations |#)
